135a14fb06f983d78389f3667190e7aebcb5e8b1
davidgeorgebell <bell.dave93@gmail.com>
2019-10-03 14:28:01 +0800

    pagination arrows swapped around

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -61,20 +61,21 @@ class BlogPostTemplate extends React.Component {
               padding: 0,
             }}
           >
+             <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                ← {next.frontmatter.title} 
+                </Link>
+              )}
+            </li>
             <li>
               {previous && (
                 <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
+                  {previous.frontmatter.title} →
                 </Link>
               )}
             </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
+            
           </ul>
         </nav>
       </Layout>
